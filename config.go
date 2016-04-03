20bc053a240451aa7cb70c198f619db4e102f5a5
orivil <307509265@qq.com>
2016-04-03 13:46:38 +0800

    新文件:       config.go 	新文件:       example_test.go 	新文件:       testdata/i18n.yml

---
+++ config.go
@@ -0,0 +1,67 @@
+package config
+
+import (
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"path/filepath"
+)
+
+func NewConfig(readDir string) *Config {
+
+	return &Config {
+		readDir: readDir,
+		cfgT: make(map[string]interface{}, 10),
+	}
+}
+
+type Config struct {
+
+	cfgT map[string]interface{}
+	readDir string
+}
+
+func (this *Config) SetDir(readDir string) {
+
+	this.readDir = readDir
+}
+
+func (this *Config) ReloadAll() {
+
+	for name, _ := range this.cfgT {
+		this.ReloadFile(name)
+	}
+}
+
+func (this *Config) ReloadFile(name string) {
+
+	inst := this.cfgT[name]
+	delete(this.cfgT, name)
+	this.ReadStruct(name, inst)
+}
+
+// try to read the file data to struct, if file not exist, this goes no error,
+// every struct should have default values
+func (this *Config) ReadStruct(name string, struc interface{}) error {
+
+	if _struc, ok := this.cfgT[name]; !ok {
+		file := filepath.Join(this.readDir, name)
+		data, err := ioutil.ReadFile(file)
+		if err != nil {
+			return err
+		}
+
+		switch filepath.Ext(name) {
+		case ".yaml", ".yml", ".json":
+			err = yaml.Unmarshal(data, struc)
+			if err != nil {
+				return err
+			}
+		}
+
+		this.cfgT[name] = struc
+	} else {
+		struc = _struc
+	}
+	return nil
+}
+
---
+++ example_test.go
@@ -0,0 +1,29 @@
+package config_test
+import (
+	"fmt"
+	"github.com/orivil/config"
+)
+
+var configI18n = &struct {
+	Languages map[string]string
+}{
+	// set default value
+	Languages: map[string]string{
+		"简体中文": "zh-CN",
+		"english": "en",
+	},
+}
+
+func ExampleConfig() {
+	// 1. new config instance
+	cfg := config.NewConfig("./testdata")
+
+	// 2. read file data to struct
+	cfg.ReadStruct("i18n.yml", configI18n)
+
+	// 3. get config data
+	fmt.Println(configI18n.Languages)
+
+	// Output:
+	// map[简体中文:zh-CN english:en]
+}
